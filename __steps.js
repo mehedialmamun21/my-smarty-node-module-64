fd6b907c436b9683e89b60fb446210c30b76caf7
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-04-29 19:40:00 +0600

    steps.js modified

--- __steps.js
+++ __steps.js
@@ -4,7 +4,7 @@
  * -------------------------
  * 1. create a folder (manually or mkdir)
  * 2. open command line to that folder
- * 3. npm init
+ * 3. npm init -y (-y is for answering yes to all questions.)
  * 4. npm install express
  * 5. create index.js
  * 
