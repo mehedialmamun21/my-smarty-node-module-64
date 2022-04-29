fa81bb336239ff0d756fa231a6633408f3556dde
mehedialmamun21 <mehedialmamuncse@gmail.com>
2022-04-29 19:48:11 +0600

    again modified steps

--- __steps.js
+++ __steps.js
@@ -5,7 +5,7 @@
  * 1. create a folder (manually or mkdir)
  * 2. open command line to that folder
  * 3. npm init -y (-y is for answering yes to all questions.)
- * 4. npm install express
+ * 4. npm install express (npm i express cors mongodb)
  * 5. create index.js
  * 
  * --------------------------
